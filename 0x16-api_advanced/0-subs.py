4e40f776ab549a9612c3af51e2a74302a859fdb4
debee18 <monoteolaoluwasubomideborah@gmail.com>
2023-09-15 00:49:46 +0000

    advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,27 +2,30 @@
 """
 0-subs
 """
+
 import requests
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'my_user_agent'}  # Set a custom User-Agent to avoid Too Many Requests error
-    response = requests.get(url, headers=headers, allow_redirects=False)  # Disable redirects to handle invalid subreddits
+    """Returns the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "my_user_agent"}
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data["data"]["subscribers"]
         return subscribers
     else:
         return 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
         subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)
 
