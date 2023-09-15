77491a23a388af6a47de58830462f002561f213c
debee18 <monoteolaoluwasubomideborah@gmail.com>
2023-09-15 00:28:51 +0000

    advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
+0-subs
 """
-
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "subreddit_subscriber_counter/1.0"}
-    
-    response = requests.get(url, headers=headers)
-    
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'my_user_agent'}  # Set a custom User-Agent to avoid Too Many Requests error
+    response = requests.get(url, headers=headers, allow_redirects=False)  # Disable redirects to handle invalid subreddits
+
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
 
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        num_subscribers = number_of_subscribers(subreddit)
+        print(num_subscribers)
 
